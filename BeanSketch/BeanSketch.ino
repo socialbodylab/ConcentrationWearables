6a7be20d742f5e3ff8b3eb8259c91f8b9206803a
iColpitts <izzieccampbell@gmail.com>
2015-08-05 17:50:01 -0400

    pulse and working

--- BeanSketch/BeanSketch.ino
+++ BeanSketch/BeanSketch.ino
@@ -17,112 +17,102 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(25, PIN, NEO_GRB + NEO_KHZ800);
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
-int cCon[] = {255, 104, 0};
-int cRelax[] = {102, 255, 255};
+int cRelax[] = {0, 255, 0};
+int cCon[] = {0, 0, 255};
+
+int Gn = 0;
+int Bn = 0;
 
 bool warmed = false;
 bool cooled = false;
 int n=100;
 
-long num = 0;
+long num = 10;
 
-void setup() {
+void setup() 
+{
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
   //#if defined (__AVR_ATtiny85__)
   //  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
   //#endif
   // End of trinket special code
-
+  
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
 
-void loop() {
-  
+void loop() 
+{ 
+  //read data from bean
   num = Bean.readScratchNumber(1);
-  //Bean.setScratchNumber(2, num);
+  Bean.setScratchNumber(2, num);
   
-  if ( num > 5)
+  //map data to LED color
+  Gn = map(num, 0, 10, 50, 5);
+  Bn = map(num, 0, 10, 5, 50);
+  
+  //sparkle when below Concentration Threshold™
+  if ( num < 5) 
   {
-    if (!warmed)
+    //set sparkle variables
+    int numChange = random(15,25);
+    int numChase = 3;
+  
+    //begin sparkle chase pattern
+    for (int q=0; q < numChase; q++) 
     {
-       warm();
-       warmed = true;
-       cooled = false;
+      for (int i=0; i < strip.numPixels(); i=i+numChase) 
+      {
+       strip.setPixelColor(i+q, 0, Gn, Bn);    //turn every third pixel on
+      }
+      strip.show();
+  
+      delay(50);
+  
+      for (int i=0; i < strip.numPixels(); i=i+numChase) 
+      {
+        strip.setPixelColor(i+q, 0, Gn + numChange, Bn);        //turn every third pixel off
+      }
+      //strip.show();
     }
-    concentrated();
-    Bean.setScratchNumber(2, (num + 1));
+    delay(50);
   }
-  if (num <= 5) 
+  
+  //begin pulse when above Concentration Threshold™
+  else 
   {
-    if (!cooled)
+    //set fade intensity
+    int numFade = 35;
+    
+    //begin fade out
+    for(int i=0; i<numFade; i++)
     {
-      cool();
-      warmed = false;
-      cooled = true;
-    }    
-    relaxed();
-    Bean.setScratchNumber(2, 1);
-  }
+       for (int q=0; q < strip.numPixels(); q++)
+       {
+         strip.setPixelColor(q, 0, Gn, Bn - i);
+       }
   
-  Bean.sleep(50);
-
-}
-
-void warm() {
-  
-  for(int i = 0; i < n; i++) // larger values of 'n' will give a smoother/slower transition.
-  {
-     int Rn = cRelax[0] + (cCon[0] - cRelax[0]) * i / n;
-     int Gn = cRelax[1] + (cCon[1] - cRelax[1]) * i / n;
-     int Bn = cRelax[2] + (cCon[2] - cRelax[2]) * i / n;
-     
-     //serial.println( Rn 
-   
-    for(int i = 0; i < strip.numPixels();i++) {
-      strip.setPixelColor(i, strip.Color(Rn, Gn, Bn));
       strip.show();
-      delay(10);
+      delay(100);
     }
-  }
-
-}
-
-void concentrated() {
+    
+    delay(500);
+    
+    //being fade in
+    for (int i=0; i<numFade; i++)
+    {
+      for (int q=0;q < strip.numPixels(); q++)
+      {
+         strip.setPixelColor(q, 0, Gn, (Bn - numFade) + i);
+      }
   
-  for(int i = 0; i < strip.numPixels();i++) {
-    strip.setPixelColor(i, strip.Color(cCon[0], cCon[1],cCon[2]));
-    strip.show();
-    delay(100);
-  }
-  
-}
-
-void cool() {
-  
-  for(int i = 0; i < n; i++) // larger values of 'n' will give a smoother/slower transition.
-  {
-     int Rn = cCon[0] + (cRelax[0] - cCon[0]) * i / n;
-     int Gn = cCon[1] + (cRelax[1] - cCon[1]) * i / n;
-     int Bn = cCon[2] + (cRelax[2] - cCon[2]) * i / n;
-     
-     //serial.println( Rn 
-   
-    for(int i = 0; i < strip.numPixels();i++) {
-      strip.setPixelColor(i, strip.Color(Rn, Gn, Bn));
       strip.show();
-      delay(10);
+      delay(100);
     }
+    
+    delay(300);
   }
   
-}
+  delay(10);
 
-void relaxed() {
-  
-  for(int i = 0; i < strip.numPixels();i++) {
-    strip.setPixelColor(i, strip.Color(cRelax[0], cRelax[1], cRelax[2]));
-    strip.show();
-    delay(100);
-  }
-  
 }
